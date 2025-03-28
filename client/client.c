1d6ddf58a97212109d20cb8619bfb024432e507c
TOTHTOT <37585883+TOTHTOT@users.noreply.github.com>
2025-03-28 16:21:23 +0800

    添加功能
    1. 增加client的基本数据解析;

--- client/client.c
+++ client/client.c
@@ -1,1 +1,135 @@
 #include "client.h"
+#include "../log.h"
+#include <cjson/cJSON.h>
+
+// 生成 JSON
+/**
+ * @description: 将结构体数据序列化为 JSON 字符串, 一般客户端使用这个函数
+ * @param {ele_client_info_t} *client_info 客户端信息结构体
+ * @return {char *} JSON 字符串
+ * @note: 需要调用者释放内存, 使用 free(json_str) 释放
+ */
+char *client_serialize_to_json(const ele_client_info_t *client_info)
+{
+    cJSON *root = cJSON_CreateObject();
+
+    // 添加消息类型
+    cJSON_AddNumberToObject(root, "type", client_info->type);
+
+    // 传感器数据
+    cJSON *sensor = cJSON_CreateObject();
+    cJSON_AddNumberToObject(sensor, "temperature", client_info->sensor_data.temperature);
+    cJSON_AddNumberToObject(sensor, "humidity", client_info->sensor_data.humidity);
+    cJSON_AddNumberToObject(sensor, "pressure", client_info->sensor_data.pressure);
+    cJSON_AddNumberToObject(sensor, "tvoc", client_info->sensor_data.tvoc);
+    cJSON_AddNumberToObject(sensor, "co2", client_info->sensor_data.co2);
+    cJSON_AddItemToObject(root, "sensor_data", sensor);
+
+    // 客户端配置
+    cJSON *config = cJSON_CreateObject();
+    cJSON_AddStringToObject(config, "username", client_info->cfg.username);
+    cJSON_AddStringToObject(config, "cityname", client_info->cfg.cityname);
+    cJSON_AddNumberToObject(config, "cityid", client_info->cfg.cityid);
+    cJSON_AddNumberToObject(config, "cntserver_interval", client_info->cfg.cntserver_interval);
+    cJSON_AddNumberToObject(config, "version", client_info->cfg.version);
+    cJSON_AddNumberToObject(config, "battery", client_info->cfg.battery);
+    cJSON_AddItemToObject(root, "cfg", config);
+
+    // 转换为 JSON 字符串
+    // char *json_str = cJSON_Print(root);
+    char *json_str = cJSON_PrintUnformatted(root); // 不格式化输出
+    cJSON_Delete(root);
+    return json_str;
+}
+
+/**
+ * @description: 将 JSON 字符串反序列化为结构体数据, 一般服务器使用这个函数
+ * @param {char} *json_str JSON 字符串
+ * @param {ele_client_info_t} *client_info 客户端信息结构体
+ * @return {int} 0:成功 -1:失败
+ */
+int client_deserialize_from_json(const char *json_str, ele_client_info_t *client_info)
+{
+    cJSON *root = cJSON_Parse(json_str);
+    if (root == NULL)
+        return -1;
+
+    // 解析消息类型
+    cJSON *type = cJSON_GetObjectItem(root, "type");
+    if (type && cJSON_IsNumber(type)) {
+        client_info->type = type->valueint;
+    } else {
+        cJSON_Delete(root);
+        return -1; // 缺少或无效的类型字段
+    }
+
+    // 解析传感器数据
+    cJSON *sensor = cJSON_GetObjectItem(root, "sensor_data");
+    if (sensor)
+    {
+        client_info->sensor_data.temperature = cJSON_GetObjectItem(sensor, "temperature")->valueint;
+        client_info->sensor_data.humidity = cJSON_GetObjectItem(sensor, "humidity")->valueint;
+        client_info->sensor_data.pressure = cJSON_GetObjectItem(sensor, "pressure")->valueint;
+        client_info->sensor_data.tvoc = cJSON_GetObjectItem(sensor, "tvoc")->valueint;
+        client_info->sensor_data.co2 = cJSON_GetObjectItem(sensor, "co2")->valueint;
+    }
+
+    // 解析客户端配置
+    cJSON *config = cJSON_GetObjectItem(root, "cfg");
+    if (config)
+    {
+        strncpy(client_info->cfg.username, cJSON_GetObjectItem(config, "username")->valuestring, USER_NAME_SIZE);
+        strncpy(client_info->cfg.cityname, cJSON_GetObjectItem(config, "cityname")->valuestring, CITY_NAME_SIZE);
+        client_info->cfg.cityid = cJSON_GetObjectItem(config, "cityid")->valueint;
+        client_info->cfg.cntserver_interval = cJSON_GetObjectItem(config, "cntserver_interval")->valueint;
+        client_info->cfg.version = cJSON_GetObjectItem(config, "version")->valueint;
+        client_info->cfg.battery = cJSON_GetObjectItem(config, "battery")->valueint;
+    }
+
+    cJSON_Delete(root);
+    return 0;
+}
+
+#if 0
+// gcc -o client client.c -lcjson # 编译命令
+int main()
+{
+    ele_client_info_t client_info = {
+        .type = 1,
+        .sensor_data = {25, 60, 101325, 50, 400},
+        .cfg = {"test_user", "Beijing", 110000, 30, 20240328, 85},
+    };
+
+    // 生成 JSON
+    char *json_str = client_serialize_to_json(&client_info);
+    printf("Generated JSON:\n%s\n", json_str);
+
+    // 解析 JSON
+    ele_client_info_t new_client_info;
+    memset(&new_client_info, 0, sizeof(new_client_info));
+    if (client_deserialize_from_json(json_str, &new_client_info) == 0)
+    {
+        printf("\nParsed Data:\n");
+        printf("Type: %u\n", new_client_info.type);
+        printf("Username: %s\n", new_client_info.cfg.username);
+        printf("City: %s\n", new_client_info.cfg.cityname);
+        printf("City ID: %u\n", new_client_info.cfg.cityid);
+        printf("Interval: %u sec\n", new_client_info.cfg.cntserver_interval);
+        printf("Version: %u\n", new_client_info.cfg.version);
+        printf("Battery: %u%%\n", new_client_info.cfg.battery);
+        printf("Temperature: %u°C\n", new_client_info.sensor_data.temperature);
+        printf("Humidity: %u%%\n", new_client_info.sensor_data.humidity);
+        printf("Pressure: %u Pa\n", new_client_info.sensor_data.pressure);
+        printf("TVOC: %u ppb\n", new_client_info.sensor_data.tvoc);
+        printf("CO2: %u ppm\n", new_client_info.sensor_data.co2);
+    }
+    else
+    {
+        printf("Failed to parse JSON\n");
+    }
+
+    free(json_str); // 释放 JSON 字符串
+    return 0;
+}
+#endif
+
--- client/client.h
+++ client/client.h
@@ -5,16 +5,42 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <string.h>
 
 /* 宏定义 */
 #define USER_NAME_SIZE 20 // 用户名最大长度
 #define CITY_NAME_SIZE 20 // 城市名字最大长度
 /* 类型定义 */
+
+typedef struct
+{
+    uint8_t temperature;  // 温度
+    uint8_t humidity;     // 湿度
+    uint32_t pressure;    // 气压
+    uint16_t tvoc;         // VOC
+    uint16_t co2;          // CO2
+} ele_client_sensor_data_t; // 客户端传感器数据结构体
+
 typedef struct
 {
     char username[USER_NAME_SIZE];
     char cityname[CITY_NAME_SIZE];
-}ele_client_info;
+    uint32_t cityid;             // 城市ID 和 cityname 对应, 查询天气使用
+    uint16_t cntserver_interval; // 连接服务器间隔时间
+    uint32_t version;            // 客户端版本号
+    uint8_t battery;             // 电池电量
+} ele_client_cfg_t;              // 客户端配置结构体
 
+typedef struct
+{
+    uint16_t type; // 消息类型
+    ele_client_sensor_data_t sensor_data; // 客户端本地传感器采集到的数据
+    ele_client_cfg_t cfg;                 // 客户端配置
+} ele_client_info_t;                      // 客户端连接到服务器要发送过来的消息
+
+
+/* 函数 */
+extern char *client_serialize_to_json(const ele_client_info_t *client_info); // 将结构体数据序列化为 JSON 字符串
+extern int client_deserialize_from_json(const char *json_str, ele_client_info_t *client_info); // 将 JSON 字符串反序列化为结构体数据
 
 #endif /* __CLIENT_H__ */
