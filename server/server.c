d9a25896a8d4aca927134fbe79d675fb96554515
TOTHTOT <37585883+TOTHTOT@users.noreply.github.com>
2025-05-08 14:09:12 +0800

    修复bug
    1. 删除 server.c  重复代码;

--- server/server.c
+++ server/server.c
@@ -400,14 +400,10 @@ static int8_t client_events(server_t *server, int32_t i)
             if (ret < 0) // 处理客户端事件
             {
                 LOG_W("client_event_handler failed, ret = %d", ret);
-                LOG_W("client_event_handler failed, ret = %d", ret);
             }
             ret = handle_client_msg(server, i, &client_msg);
             if (ret != 0)
-            ret = handle_client_msg(server, i, &client_msg);
-            if (ret != 0)
             {
-                LOG_W("handle_client_msg failed, ret = %d", ret);
                 LOG_W("handle_client_msg failed, ret = %d", ret);
             }
         }
